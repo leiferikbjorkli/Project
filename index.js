dddfbc4b141ecd003288ae13a55896236d9f075c
LeifErik <leiferikbjorkli@gmail.com>
2016-10-01 17:05:23 +0200

    test4

--- index.js
+++ index.js
@@ -12,8 +12,48 @@ var routes = require('./lib/routes');
 var config = require('./shared/lib/config');
 var util = require('util');
 
+// var app = express();
+// app.set('port', config.port);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html');
+// app.engine('html', consolidate.handlebars);
+// app.use(favicon(__dirname + '/static/images/favicon.ico'));
+// app.use(morgan('combined'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(methodOverride());
+// app.use(cookieParser());
+
+
+// app.use('/static', express.static(path.join(__dirname, 'static')));
+
+// if ('development' == app.get('env')) {
+//     app.use(errorhandler());
+// }
+
+// app.use('/', routes);
+
+// // Handle health check
+// app.use('/_health', function(req, res) {
+//     res.send('200 I am alive', 200);
+// });
+
+// // Handle 404
+// app.use(function (req, res) {
+//     util.log('404: req.method=' + req.method + ', req.url=' + req.url);
+//     res.send('404 Not Found', 404);
+// });
+
+// // Handle 500
+// app.use(function (error, req, res, next) {
+//     util.error('500: error=' + error + ', req.method=' + req.method + ', req.url=', req.url);
+//     res.send('500 Internal Server Error', 500);
+// });
+
+var port = process.env.PORT || 3000;
+
 var app = express();
-app.set('port', config.port);
+app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', consolidate.handlebars);
@@ -26,30 +66,9 @@ app.use(cookieParser());
 
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
-
-if ('development' == app.get('env')) {
-    app.use(errorhandler());
-}
-
 app.use('/', routes);
 
-// Handle health check
-app.use('/_health', function(req, res) {
-    res.send('200 I am alive', 200);
-});
 
-// Handle 404
-app.use(function (req, res) {
-    util.log('404: req.method=' + req.method + ', req.url=' + req.url);
-    res.send('404 Not Found', 404);
-});
-
-// Handle 500
-app.use(function (error, req, res, next) {
-    util.error('500: error=' + error + ', req.method=' + req.method + ', req.url=', req.url);
-    res.send('500 Internal Server Error', 500);
-});
-
-app.listen(app.get('port'), function () {
-    util.log('Server listening on port ' + app.get('port'));
+app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
